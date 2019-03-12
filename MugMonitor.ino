fae4548b6e334ae40317d8921d3ce48b119a41eb
Sam Faull <samz5991@hotmail.co.uk>
2019-03-12 22:14:00 +0000

    Added new reminder state to pulse green if optimum temperature is reached. Added isObjectRemoved function but doesn;t work quite properly so hadn't been incorporated in the state machine

--- MugMonitor.ino
+++ MugMonitor.ino
@@ -3,6 +3,9 @@
  *    cycling through all states.
  *  - In the "running state", colour always seems to go to red first and then drop back to blue (when the object is cold)
  *  
+ *  Feature requests:
+ *  - While in standby, collect ambient temperature data and dynamicallt set off_threshold based on temperature history
+ *  - Some sort of "optimum drinking temperature" animation or alarm
  */
 
 
@@ -12,7 +15,7 @@
 
 #define NUM_LEDS 12
 #define DATA_PIN 1
-#define LED_UPDATE_TIMEOUT 10
+#define LED_UPDATE_TIMEOUT 5
 #define STARTUP_ANIMATION_DURATION 500 // 0.5 seconds
 #define SWEEP_TIMEOUT 30
 
@@ -31,17 +34,18 @@ typedef enum {
     kStandby = 0,
     kStartup = 1,
     kRunning = 2,
-    kShutdown = 3
+    kReminder = 3,
+    kShutdown = 4
 } state_t;
 
 state_t currentState;
 
-double temp_ambient = 0;
-double temp_object = 0;
-double temp_max = 70.0; //deg C
-double temp_min = 50.0; //deg C
-double cold2hold_threshold = 50.0;  // not considered drinkable below this temp.
-double off_threshold = 21.0;  // assumed to be no cup presetent below this temperature
+// all temperatures are scaled up by 10
+uint16_t temp_ambient = 0;
+uint16_t temp_object = 0;
+uint16_t temp_max = 700; // 70.0 deg C
+uint16_t temp_min = 400; // 40.0 deg C
+
 
 void setup() 
 {
@@ -66,6 +70,9 @@ void loop()
           break;
       case kRunning:
           currentState = do_Running();
+          break;
+      case kReminder:
+          currentState = do_Reminder();
           break;
       case kShutdown:
           currentState = do_Shutdown();
@@ -106,6 +113,13 @@ state_t transition_D(void)
     return kStandby;
 }
 
+state_t transition_E(void)
+{
+  // start animation
+  setTimer(&animationTimerA); // reset timer
+  return kReminder;
+}
+
 state_t do_Standby(void)
 {
   //Serial.println("Standby");
@@ -115,8 +129,6 @@ state_t do_Standby(void)
     return transition_A();
   return kStandby;
 }
-
-
 
 state_t do_Startup(void)
 {
@@ -160,14 +172,21 @@ state_t do_Running(void)
     //Serial.println("No object present!");
     return transition_C();
   }
+
+  // if optimum temperature, do a little animation
+  if(temp_object > 50 && temp_object < 60)
+  {
+    // TODO: needs some sort of timer on this, dont want it going off all the time
+    return transition_E();
+  }
+  
   //Serial.println("Object present!");
   // update the LED colours based on temp
   if(timerExpired(ledTimer, LED_UPDATE_TIMEOUT))
   {
     setTimer(&ledTimer); // reset timer
 
-    int r,g,b;
-    g = 0;
+    uint8_t r,b;
     
     if (temp_object < temp_min)
       temp_object=temp_min;
@@ -178,11 +197,41 @@ state_t do_Running(void)
     r = map(temp_object, temp_min, temp_max, 0, BRIGHTNESS);
     b = map(temp_object, temp_min, temp_max, BRIGHTNESS, 0);
   
-    SetColourTargetAll(r,g,b);
+    SetColourTargetAll(r,0,b);
   }
 
 
   return kRunning;
+}
+
+
+state_t do_Reminder(void)
+{
+  static bool targetMet = false;
+  static uint8_t brightness = BRIGHTNESS;
+  //Serial.println("Reminder");
+
+  if(timerExpired(animationTimerA, 5))
+  {
+    setTimer(&animationTimerA); // reset timer
+    // TODO: animate a green pulse to indicate good drinking temperature
+    targetMet = SetColourTargetAll(0,brightness, 0);
+  }
+
+  if(targetMet)
+  {
+    targetMet = false;
+    
+    if (brightness == BRIGHTNESS)
+      brightness = 0;
+    else if (brightness == 0)
+    {
+      brightness = BRIGHTNESS;
+      return transition_B();
+    }
+  }
+
+  return kReminder;
 }
 
 state_t do_Shutdown(void)
@@ -203,15 +252,17 @@ state_t do_Shutdown(void)
 void updateReadings(void)
 {
   // read the sensor
-  temp_ambient = mlx.readAmbientTempC();
-  temp_object = mlx.readObjectTempC();
+  temp_ambient = (uint16_t)(mlx.readAmbientTempC()*10);
+  temp_object = (uint16_t)(mlx.readObjectTempC()*10);
 }
 
 /* object is assumed present if there is a large enough positive delta between object temp and ambient temp */
 bool isObjectPresent(void)
 {
-  const double threshold = 2.0;
-  double temp_delta = temp_object - temp_ambient;
+  const uint8_t off_threshold = 240;  // assumed to be no cup presetent below this temperature
+  const uint8_t threshold = 20;
+  uint16_t temp_delta = temp_object - temp_ambient;
+  
   // check delta is large enough and that object temperature exceeds minimum temperature
   // TODO: should also be a condition in here to check that  temp_object > temp_min
   if (temp_delta > threshold && temp_object > off_threshold)
@@ -220,17 +271,32 @@ bool isObjectPresent(void)
   return false;
 }
 
+// assumes aboject was present in the first place
+bool isObjectRemoved(void)
+{
+  const uint8_t off_threshold = 210;  // assumed to be no cup presetent below this temperature
+  const uint8_t threshold = 20;
+  uint16_t temp_delta = temp_object - temp_ambient;
+  
+  // check delta is large enough and that object temperature exceeds minimum temperature
+  // TODO: should also be a condition in here to check that  temp_object > temp_min
+  if (temp_delta < threshold && temp_object < off_threshold)
+    return true;
+
+  return false;
+}
+
 /* pass this function a pointer to an unsigned long to store the start time for the timer */
 void setTimer(unsigned long *startTime)
 {
-  *startTime = millis();;  // store the current time
+  *startTime = millis();  // store the current time
 }
 
 /* call this function and pass it the variable which stores the timer start time and the desired expiry time 
    returns true fi timer has expired */
 bool timerExpired(unsigned long startTime, unsigned long expiryTime)
 {
-  if ( (millis() - startTime) >= expiryTime )
+  if( (millis() - startTime) >= expiryTime )
     return true;
   return false;
 }
@@ -246,22 +312,27 @@ void SetColourAll(uint8_t r, uint8_t g, uint8_t b)
   currentColour[1] = g;
   currentColour[2] = b;
   
-  for (int i=0; i<NUM_LEDS; i++)
+  for (uint8_t i=0; i<NUM_LEDS; i++)
     pixel.setPixelColor(i, pixel.Color(r,g,b));  
 }
 
-void SetColourTargetAll(uint8_t r, uint8_t g, uint8_t b)
+bool SetColourTargetAll(uint8_t r, uint8_t g, uint8_t b)
 {
   uint8_t temp[3] = {r,g,b};
+  bool targetMet = true;
 
-  for (int i= 0; i<3; i++)
+  for (uint8_t i= 0; i<3; i++)
   {
     if(currentColour[i]<temp[i])
       currentColour[i]++;
     else if (currentColour[i]>temp[i])
       currentColour[i]--;
+
+    targetMet &= (currentColour[i]==temp[i]);
   }
   
-  for (int i=0; i<NUM_LEDS; i++)
-    pixel.setPixelColor(i, pixel.Color(currentColour[0],currentColour[1],currentColour[2]));  
+  for (uint8_t i=0; i<NUM_LEDS; i++)
+    pixel.setPixelColor(i, pixel.Color(currentColour[0],currentColour[1],currentColour[2]));
+
+  return targetMet;
 }
